c8848e4bb8452ed7619de36e7a2986517fbf6430
Rodrigo Bezerra Cordeiro <87913655+rodrigobcor@users.noreply.github.com>
2023-02-07 18:56:01 -0300

    Update LevelManager.cpp

--- Projeto  2/LevelManager.cpp
+++ Projeto  2/LevelManager.cpp
@@ -27,5 +27,40 @@ void LevelManager::LoadLevel() {
 }
 
 void LevelManager::UpdateLevel() {
-	currentLevel++;
+	if (currentLevel == 3) {
+		currentLevel = 3;
+	}
+	else {
+		currentLevel++;
+	}
+}
+
+void LevelManager::showText(SDL_Renderer* renderer, TTF_Font* font, SDL_Color color) {
+	levelTextSurface = TTF_RenderText_Solid(font, to_string(currentLevel).c_str(), color);
+
+	levelTextRect.x = 5;
+	levelTextRect.y = 5;
+	levelTextRect.w = levelTextSurface->w;
+	levelTextRect.h = levelTextSurface->h;
+
+	levelTextTexture = SDL_CreateTextureFromSurface(renderer, levelTextSurface);
+
+	SDL_RenderCopy(renderer, levelTextTexture, NULL, &levelTextRect);
+}
+
+void LevelManager::gameOver(SDL_Renderer* renderer, TTF_Font* font, SDL_Color color) {
+	gameOverSurface = TTF_RenderText_Solid(font, to_string(currentLevel).c_str(), color);
+
+	gameOverRect.x = levelTextSurface->w / 2;
+	gameOverRect.y = levelTextSurface->h / 2;
+	gameOverRect.w = levelTextSurface->w;
+	gameOverRect.h = levelTextSurface->h;
+
+	gameOverTexture = SDL_CreateTextureFromSurface(renderer, levelTextSurface);
+
+	SDL_RenderCopy(renderer, gameOverTexture, NULL, &gameOverRect);
+}
+
+int LevelManager::getCurrentLevel() {
+	return currentLevel;
 }
