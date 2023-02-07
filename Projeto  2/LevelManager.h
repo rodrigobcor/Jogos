41fc180d130780055ef3f7ee934e98e4004414ec
Rodrigo Bezerra Cordeiro <87913655+rodrigobcor@users.noreply.github.com>
2023-02-07 18:56:44 -0300

    Update LevelManager.h

--- Projeto  2/LevelManager.h
+++ Projeto  2/LevelManager.h
@@ -10,8 +10,17 @@ public:
 	void LoadLevel();
 	void UpdateLevel();
 	char levelMap[TILE_ROWS][TILE_COLS] = { '0' };
+	int getCurrentLevel();
+	void showText(SDL_Renderer* renderer, TTF_Font* font, SDL_Color color);
+	void gameOver(SDL_Renderer* renderer, TTF_Font* font, SDL_Color color);
 
 private:
 	ifstream levelFile;
 	int currentLevel = 1;
+	SDL_Rect levelTextRect;
+	SDL_Surface* levelTextSurface = nullptr;
+	SDL_Texture* levelTextTexture = nullptr;
+	SDL_Rect gameOverRect;
+	SDL_Surface* gameOverSurface = nullptr;
+	SDL_Texture* gameOverTexture = nullptr;
 };
